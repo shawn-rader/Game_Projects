26bd20aeff95f861915a02fb6f8e15ddc7dd9a15
Shawn Rader <shawn.rader@gmail.com>
2024-05-19 17:13:42 -0600

    upload issues still

--- server.js
+++ server.js
@@ -120,21 +120,28 @@ const upload = multer({
     logWithTimestamp(`Request body in file filter: ${JSON.stringify(req.body)}`);
     cb(null, true);
   }
-});
+}).fields([{ name: 'file', maxCount: 1 }, { name: 'uuid', maxCount: 1 }]);
 
-app.post('/uploadFile', upload.single('file'), (req, res) => {
-  const uuid = req.body.uuid;
-  logWithTimestamp(`Handling file upload for UUID: ${uuid}`);
-  logWithTimestamp(`Request body: ${JSON.stringify(req.body)}`);
-  if (!uuid) {
-    logWithTimestamp('UUID is undefined in the request body.');
-    return res.status(400).send('UUID is required');
-  }
+app.post('/uploadFile', (req, res) => {
+  upload(req, res, function (err) {
+    if (err) {
+      logWithTimestamp(`Upload error: ${err.message}`);
+      return res.status(400).send(err.message);
+    }
+
+    const uuid = req.body.uuid;
+    logWithTimestamp(`Handling file upload for UUID: ${uuid}`);
+    logWithTimestamp(`Request body: ${JSON.stringify(req.body)}`);
+    if (!uuid) {
+      logWithTimestamp('UUID is undefined in the request body.');
+      return res.status(400).send('UUID is required');
+    }
   
-  const folderPath = path.join('/home/bitnami/game_projects/Tools/ContentSharing/HostedData', uuid);
-  const fileURL = path.join(folderPath, req.file.filename);
-  logWithTimestamp(`File uploaded to: ${fileURL}`);
-  res.send(fileURL);
+    const folderPath = path.join('/home/bitnami/game_projects/Tools/ContentSharing/HostedData', uuid);
+    const fileURL = path.join(folderPath, req.files['file'][0].filename);
+    logWithTimestamp(`File uploaded to: ${fileURL}`);
+    res.send(fileURL);
+  });
 });
 
 // Delete unused folders
